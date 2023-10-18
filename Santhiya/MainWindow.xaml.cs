f54b4f0ed57bcd5d3221ba94f7beca6ca7152370
Santhiya0808 <141845257+Santhiya0808@users.noreply.github.com>
2023-10-18 17:04:39 +0530

    This is a sample color palette task

---
+++ Santhiya/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Santhiya
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
